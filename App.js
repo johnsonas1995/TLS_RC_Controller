58c024b30a47ef29a549ca890e3f9b8e3002356f
johnsonas1995 <johnsonas1995@gmail.com>
2023-01-04 12:19:02 -0500

    throttle and steering sliders, logo, and lights button added

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import * as ScreenOrientation from 'expo-screen-orientation';
 ScreenOrientation.unlockAsync()
 import Slider from '@react-native-community/slider'
-
+import { useState } from 'react';     
+import logo from './assets/tls_logo.jpg';     
+//Used RectTransform, PointerEventData, localEulerAngles for steering wheel?
+//or probably just horizontal slider that transforms image of steering wheel with transform([{rotateX: '45deg'}
 
 export default function App() {
+  const [throttleVal, setThrottleVal] = useState(0);
+  const [steerVal, setSteerVal] = useState(0);
+  const [lights, setLights] = useState(false);
+
   return (
-    
+    <>
     <View style={styles.container}>
-    <Slider style={{width:200, height:40, transform: [ { rotate: "-90deg" } ], alignSelf:'flex-start'}} minimumValue={-10} maximumValue={10}/>
+    <Image source={logo} style={{width:200, height:200, alignSelf:'center'}}/>
     
-      <Text>Open up App.js to start working on your app!</Text>
+    <Text style={{alignSelf:'flex-start'}}>   Speed: {throttleVal}</Text>
+
+    <Slider style={{width:200, height:40, transform: [ { rotate: "-90deg" } ], alignSelf:'flex-start'}} //could use wheel picker instead
+      minimumValue={-10} 
+      maximumValue={10}
+      onValueChange={(value) => setThrottleVal(value)}
+      step={1}
+      value={throttleVal}
+      />
+    
+      
+
+      <Slider style={{width:200, height:40, alignSelf:'flex-end'}} //could use wheel picker instead
+      minimumValue={-1} 
+      maximumValue={1}
+      onValueChange={(value) => setSteerVal(value)}
+      step={1}
+      value={steerVal}
+      />
+
+    <Text style={{alignSelf:'flex-end'}}>   Steering: {steerVal}   </Text>
+    <Button
+        style={{position:'absolute', bottom:0, left:0}}
+        onPress={setLights}
+        title="Lights"
+        color="#40E0D0"
+        accessibilityLabel="Toggle Lights"
+      />
     
       <StatusBar style="auto" />
     </View>
+    
+    </>
   );
 }
 
